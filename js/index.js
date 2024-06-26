d38f65c62cea777ff9294529213e011426dc20c6
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-06-26 18:22:50 +0600

    js done

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ tl.fromTo('.cookie-container',{scale:0}, {scale:1, ease: "elastic.out(1,0.4)", d
 tl.fromTo('.cookie',{opacity:0, x:-50, rotation:'-45deg'}, {opacity:1 , x:0, rotation:'0deg'}, '<50%');
 tl.fromTo('.text', {x:30, opacity:0}, {x:0, opacity:1}, '<');
 
+// cookie jump 
+tl.fromTo('.cookie', {y:0, rotation:0}, {y:-20, rotation:'-20deg', yoyo:true, repeat:-1})
+tl.fromTo('#crums', {y:0, rotation:0}, {y:-20, yoyo:true, repeat:-1},'>')
 // event
 
 const btn = document.getElementById('btn-cookie');
